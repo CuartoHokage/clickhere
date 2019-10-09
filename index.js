6775e12eb3189c9d1205f11cb0944fb112bd051b
CuartoHokage <jaime199505@hotmail.com>
2019-10-09 17:54:45 -0500

    fin del di

--- index.js
+++ index.js
@@ -15,50 +15,38 @@ const config= require('./config')
 // })
 
 ////// conexion a sql server
+
+
+
+///
+const mssl = require('mssql')
+
+
 const config2 = {
     user: 'sa',
-    password: 'clickhere2018',
+    password: 'Clickhere2018',
     server: '192.168.2.102', // You can use 'localhost\\instance' to connect to named instance
     database: 'click',
- 
+ /*
     options: {
         encrypt: true // Use this if you're on Windows Azure
-    }
+	}
+	*/
 }
-///
-/*
-const sql = require('mssql')
 
-
-(async function () {
-    try {
-        let pool = await sql.connect(config2)
-        let result1 = await pool.request()
-            .input('input_parameter', sql.Int, value)
-            .query('select * from mytable where id = @input_parameter')
-            
-        console.dir(result1)
-    
-        // Stored procedure
-        
-        let result2 = await pool.request()
-            .input('input_parameter', sql.Int, value)
-            .output('output_parameter', sql.VarChar(50))
-            .execute('procedure_name')
-        
-        console.dir(result2)
-    } catch (err) {
-        // ... error checks
-    }
-})()
- 
-sql.on('error', err => {
-    // ... error handler
-})
-//////
 console.log("sdsd")
+var connection = mssl.connect(config2, function(err, res){
+	if(err){
+		throw err;
 
-*/
+	} else{
+		console.log("conectado a la base de datos");
+	}
+});
+//
+var request =new mssl.Request();
+request
+//
 app.listen(config.port, ()=>{
 	console.log(`Api Rest corriendo en el puerto ${config.port}`)
 })
