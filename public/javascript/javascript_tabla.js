849763d93cc283d76ec13532869313d1d8e69a15
CuartoHokage <jaime199505@hotmail.com>
2019-10-11 11:37:35 -0500

    actualización de alearta de pedido de cotización

--- public/javascript/javascript_tabla.js
+++ public/javascript/javascript_tabla.js
@@ -1,37 +1,15 @@
 $(document).ready(function () {
     
-            $("#contenido").html(htmlpuro)
-            $(".btn-outline-danger").click(function(){
+            //$("#contenido").html(htmlpuro)
+            $(".btn-info").click(function(){
                 var esteBoton= $(this);
                 var id = esteBoton.parent().parent().find("#id_producto").text();
                 var name=esteBoton.parent().parent().find("#name").text();
                 swal({
-                    title: "¿Deseas eliminar la película?",
-                    text: "Eliminaras la película \""+name+"\" ",
-                    icon: "warning",
+                    title: "Gracias por contactarnos",
+                    text: "Su petición fue enviada en breve nos contactaremos con usted.",
+                    icon: "success",
                     buttons: true,
-                    dangerMode: true,
                   })
-                  .then((willDelete) => {
-                    if (willDelete) {
-                      swal("La película a sido eliminada correctamente", {
-                        icon: "success",
-                      });
-                      $.ajax({
-                        type:"DELETE",
-                        url:"/api/pelicula/"+id,
-                        dataType:"text",
-                        contentType:"application/json" 
-                    }).done(function(msg){
-                        //alert(msg);
-                        esteBoton.parent().parent().remove();
-                    });
-                    } else {
-                      swal("Ok no eliminaremos nada.");
-                    }
-                  });
-                
-                    
-                
              }); 
         });
