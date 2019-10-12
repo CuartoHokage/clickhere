9e6cd11d2cc15fa2799e207013403ba0cb4cd441
CuartoHokage <jaime199505@hotmail.com>
2019-10-12 09:33:40 -0500

    inicio de 12/10/2019

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ const nodemailer = require('nodemailer')
 
 
 ///
-const mssl = require('mssql')
+const mssql = require('mssql')
 
 
 const config2 = {
     user: 'sa',
     password: 'Clickhere2018',
     server: '192.168.2.102', // You can use 'localhost\\instance' to connect to named instance
-    database: 'click',
+    database: 'click'
  /*
     options: {
         encrypt: true // Use this if you're on Windows Azure
@@ -36,7 +36,7 @@ const config2 = {
 }
 
 console.log("sdsd")
-var connection = mssl.connect(config2, function(err, res){
+var connection = mssql.connect(config2, function(err, res){
 	if(err){
 		throw err;
 
@@ -48,53 +48,69 @@ var connection = mssl.connect(config2, function(err, res){
 
 // inicioobtencion de datos en un array de productos
 
-/*
-var request =new mssl.Request();
+
+var request =new mssql.Request();
 //revisar https://www.youtube.com/watch?v=X2Stnj3eVls
-request.query('SELECT * from dbo.MAE_PRODUCTO', function(err, result){
-	//if(err)
-		//return next(err);
+//const result = await sql.query`select * from mytable where id = ${value}`
+
+
+/*
+request.query('select * FROM [click].[dbo].[MAE_PRODUCTO]', function(err, result){
+  //console.log(result.recordset)
+	if(err)
+    console.log(err);
+    
 	var data= {};
 	data= result.recordset;
-	console.log(data)
+	console.log(result.recordset)
 	res.send(data);
 	
 });
 */
 
+  
+
 ////fin obtencion de datos en un array de productos
 
+/////////////////////////////////////TEDIOUS//////////////////////////////////////////////
+
+var db = require("./controllers/dbController.js");
+var TYPES = require("tedious").TYPES;
+
+// QUERY
+// Imagine we have a table called pokemon with 2 attributes: name and number
+// We want to find a pokemon by number, using a select query
+const getPokemon = (number) => {
+  var params = [];
+  var sql = "select * from pokemon where number = @number";
+  // For each param do: db.buildParams(params, "name", TYPES.type, variable)
+  db.buildParams(params, "number", TYPES.Int, number);
+  db.query(params, sql, result => {
+    console.log(result);
+  });
+}
+
+// PROCEDURE
+// The same as above but using instead a procedure called findPokemon(number)
+const getPokemonProc = (number) => {
+  var params = [];
+  var sql = "findPokemon"; // instead of query selector use only procedure name
+  // For each param do: db.buildParams(params, "name", TYPES.type, variable)
+  db.buildParams(params, "number", TYPES.Int, number); // Params will be added automatically to the procedure exec
+  db.proc(params, sql, result => { // Just need to change from query to proc
+    console.log(result);
+  });
+}
 
 
-//FUNCIONA REACTIVAR CUANDO SE USE ENVIO DE MAILS/
-//Creamos el objeto de transporte  https://cursos.mejorcodigo.net/article/enviar-correo-electronico-con-nodejs-17
+getPokemon(132);
+getPokemonProc(34);
 
-/*
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'jaime.paz.mero@gmail.com',
-    pass: 'naruto2014'
-  }
-});
-
-var mensaje = "Hola desde nodejs...";
-
-var mailOptions = {
-  from: 'jaime.paz.mero@gmail.com',
-  to: 'jaime199505@hotmail.com',
-  subject: 'Asunto Del Correo',
-  text: mensaje
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email enviado: ' + info.response);
-  }
-});
-//FUNCIONA REACTIVAR CUANDO SE USE/ */
+module.exports = {
+  getPokemon: getPokemon,
+  getPokemonProc: getPokemonProc
+}
+/////////////////////////////////////TEDIOUS//////////////////////////////////////////////
 
 app.listen(config.port, ()=>{
 	console.log(`Api Rest corriendo en el puerto ${config.port}`)
