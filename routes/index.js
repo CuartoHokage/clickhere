6c6eb3e288ab70faa64d1d8212f4de5dfcade0ed
CuartoHokage <jaime199505@hotmail.com>
2019-11-21 18:29:33 -0500

    dddddhhbllllllb

--- routes/index.js
+++ routes/index.js
@@ -58,8 +58,33 @@ api.get('/edicion', (req, res) => {
 	res.render('admin_imagenes');
 });
 
-api.get('/carrito', ()=>{
-	res.render('carrito');
+api.get('/carrito', (req, res)=>{
+	new sql.Request().query("select DISTINCt p.PRDIDENTI, p.PRDNOMBRE, p.PRDNOMBRE as categoria, s.PUBSTOCK, ROUND((((PRDPVP * (select IMPPORCEN from CFG_IMPUESTOS where IMPIDENTI=1))/100)+ PRDPVP),2, 0) as PRDPVP from MAE_PRODUCTO p\
+	  INNER join REL_PRODUBIC s on p.PRDIDENTI= s.PRDCODIGO\
+	  where PRDNOMBRE like 'port.%' and PUBSTOCK >0 and PUBIDUBIC=12\
+	  union all\
+	  select p.PRDIDENTI, p.PRDNOMBRE, a.NOMBRE as categoria, s.PUBSTOCK, ROUND((((PRDPVP * (select IMPPORCEN from CFG_IMPUESTOS where IMPIDENTI=1))/100)+ PRDPVP),2, 0) as PRDPVP from MAE_PRODUCTO p\
+	  inner join REL_PRODAGRUPACION ra on ra.IDPRODUCTO= p.PRDIDENTI\
+	  inner join AGRUPACION a on ra.IDGRUPO= a.IDGRUPO\
+	  INNER join REL_PRODUBIC s on p.PRDIDENTI= s.PRDCODIGO and s.PRDCODIGO=p.PRDIDENTI\
+	  inner join MAE_UBICACION u on u.UBIIDENTI= s.PUBIDUBIC\
+	  where PUBSTOCK >0 and u.UBIIDENTI=12 and a.NOMBRE like 'port.%'\
+	  union all\
+	  select DISTINCt p.PRDIDENTI, p.PRDNOMBRE, Marcas.NOMBRE as categoria, s.PUBSTOCK, ROUND((((PRDPVP * (select IMPPORCEN from CFG_IMPUESTOS where IMPIDENTI=1))/100)+ PRDPVP),2, 0) as PRDPVP from MAE_PRODUCTO p\
+	  INNER join REL_PRODUBIC s on p.PRDIDENTI= s.PRDCODIGO\
+	  inner join MARCAS on MARCAS.IDENTIFICADOR= IDMARCA\
+	  where Marcas.NOMBRE like '%port%' and PUBSTOCK >0 and PUBIDUBIC=12 ", (err, result) => {
+		//handle err
+		var producto = result.recordset
+		//localStorage.setItem("producto", JSON.stringify(producto));
+		for (var i = 0; i < producto.length; i++) {
+			if (producto[i].PUBSTOCK > 5) {
+				producto[i].PUBSTOCK = "Más de 5";
+			}
+		}
+		
+		var resultado = result.recordset;
+	res.render('carrito', {data: producto});
 });
 /////
 
